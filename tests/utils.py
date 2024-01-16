5f2b75d6136d7c2e53973b8ed54c7183f9bd6305
0xfourzerofour <joshpauline@gmail.com>
2024-01-16 14:11:31 -0500

    feat(rundler): add rundler to test executor

--- tests/utils.py
+++ tests/utils.py
@@ -118,6 +118,11 @@ def assert_rpc_error(response, message, code):
     except AttributeError as exc:
         raise Exception(f"expected error object, got:\n{response}") from exc
 
+def assert_rpc_code(response, code):
+    try:
+        assert response.code == code
+    except AttributeError as exc:
+        raise Exception(f"expected error object, got:\n{response}") from exc
 
 def get_sender_address(w3, initcode):
     helper = deploy_contract(w3, "Helper")
@@ -209,8 +214,8 @@ def set_reputation(address, ops_seen=1, ops_included=2, url=None):
                 [
                     {
                         "address": address,
-                        "opsSeen": ops_seen,
-                        "opsIncluded": ops_included,
+                        "opsSeen": hex(ops_seen),
+                        "opsIncluded": hex(ops_included),
                     }
                 ],
                 CommandLineArgs.entrypoint,
