22841c9f0ddfb67570d38a0d614256ba949460a5
Alex Miao <alex.miao@alchemy.com>
2024-01-18 10:17:19 -0800

    fix(compliance): revert to original maxPriorityFeePerGas values

--- tests/types.py
+++ tests/types.py
@@ -40,7 +40,7 @@ class UserOperation:
     callGasLimit: HexStr = hex(3 * 10**5)
     verificationGasLimit: HexStr = hex(10**6)
     preVerificationGas: HexStr = hex(3 * 10**5)
-    maxFeePerGas: HexStr = hex(4 * 10**9)
+    maxFeePerGas: HexStr = hex(3 * 10**9)
     maxPriorityFeePerGas: HexStr = hex(4 * 10**9)
     paymasterAndData: HexStr = "0x"
     signature: HexStr = "0x"
